9c6e73dd3bdc478b34dc30ad58bd232ad1d38e7e
vietanh47171 <vietanh6939@gmail.com>
2023-03-28 12:44:46 +0700

    add source code

---
+++ app.js
@@ -0,0 +1,53 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const songs = [
+    {
+        name: 'Only You',
+        singer: 'The Flying Pickets',
+        path: './Music/1.mp3',
+        img:  './MusicIMG/1.jpg',
+    },
+    {
+        name: 'Forget Him',
+        singer: 'Shirley Kwan',
+        path: './Music/2.mp3',
+        img:  './MusicIMG/2.jpg',
+    },
+    {
+        name: 'Thinking About You',
+        singer: 'Wong',
+        path: './Music/3.mp3',
+        img:  './MusicIMG/3.jpg',
+    },
+    {
+        name: 'Elegy To The Void',
+        singer: 'Beach House',
+        path: './Music/4.mp3',
+        img:  './MusicIMG/4.jpg',
+    },
+    {
+        name: 'Things In Life',
+        singer: 'Dennis Brown',
+        path: './Music/5.mp3',
+        img:  './MusicIMG/5.jpg',
+    },
+    {
+        name: 'California Dreamin\' ',
+        singer: 'The Flying Pickets',
+        path: './Music/6.mp3',
+        img:  './MusicIMG/6.jpg',
+    },
+    {
+        name: 'Killer Song',
+        singer: 'Ming',
+        path: './Music/7.mp3',
+        img:  './MusicIMG/7.jpg',
+    },
+    {
+        name: 'Broque 1992',
+        singer: 'Michael Glasso',
+        path: './Music/8.mp3',
+        img:  './MusicIMG/8.jpg',
+    },
+]
